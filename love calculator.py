ef83484e6fc688a307609714a63fb32669487b71
ablueremote <a.santiago0893@gmail.com>
2023-10-30 19:39:49 -0400

    Commit updates

--- love calculator.py
+++ love calculator.py
@@ -33,3 +33,4 @@ elif score > 40 and score < 50:
   print(f"Your score is {score}, you are alright together.")
 else:
   print(f"Your score is {score}.")
+  #New Update
--- pizza_orders.py
+++ pizza_orders.py
@@ -20,3 +20,4 @@ else:
 if extra_cheese == "Y":
   price +=1
 print(f"Your final bill is: ${price}.")
+#Test
